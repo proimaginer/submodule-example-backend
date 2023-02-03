d307f2a0395e703a4a68f9f8a69b22f6f073e0ee
이정준 <jungjoon.lee@medistream.co.kr>
2023-02-03 16:35:50 +0900

    article type 추가

--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,8 @@ app.get('/', (req, res, next) => {
   res.send('ok');
 });
 
-app.get('/api/article', (req, res, next) => {
-  res.send([
+app.get('/api/articles', (req, res, next) => {
+  const articles: Article[] = [
     {
       id: 141,
       content: '첫번째 게시글'
@@ -16,7 +16,9 @@ app.get('/api/article', (req, res, next) => {
       id: 145,
       content: '두번째 게시글'
     },
-  ]);
+  ];
+
+  res.send({ articles });
 });
 
 app.listen(3030, () => {
